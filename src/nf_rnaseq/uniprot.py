84dfd3aded0914a54ad3905f710d5640cd26b3b2
jessicaw9910 <jessbwhite@gmail.com>
2024-08-07 18:19:56 -0400

    added maybe_get_gene_names method to UniProtGET

--- src/nf_rnaseq/uniprot.py
+++ src/nf_rnaseq/uniprot.py
@@ -2,6 +2,7 @@ import logging
 import os
 from dataclasses import dataclass
 
+import numpy as np
 import requests
 
 from nf_rnaseq.api_schema import APIClientGET, APIClientPOST
@@ -45,15 +46,25 @@ class UniProtGET(APIClientGET):
 
     def create_query_url(self):
         """Create URL for UniProt API query."""
-        self.url_query = os.path.join(self.url_base, self.job)
+        self.url_query = os.path.join(self.url_base, self.jobId)
 
     def maybe_get_gene_names(self):
         """Get list of gene names from UniProt ID and add as list_gene_names attr."""
-        try:
-            list_genes = [str(gene["geneName"]["value"]) for gene in self.json["genes"]]
-            self.list_gene_names = list_genes
-        except (KeyError, AttributeError) as e:
-            logging.error("Error at %s", "division", exc_info=e)
+        str_results = "results"
+        str_failedIds = "failedIds"
+
+        list_identifier = []
+        list_gene_names = []
+        if str_results in self.json.keys():
+            list_identifier.append([i["from"] for i in self.json[str_results]])
+            list_gene_names.append([i["to"] for i in self.json[str_results]])
+        if str_failedIds in self.json.keys():
+            # TODO: confirm format for multiple failed IDs (list or list of lists)
+            list_identifier.append(self.json[str_failedIds])
+            list_gene_names.append([np.nan * len(self.json[str_failedIds])])
+
+        self.list_identifier = list_identifier
+        self.list_gene_names = list_gene_names
 
     def check_if_job_ready(self, res: requests.Response):
         """Check if the job is ready."""
